99a4a66d3172d6de88201d03d4410c1eb5cd12cf
Randall Smith <rdsmith1761@gmail.com>
2023-01-26 17:58:20 -0500

    Challenge 2

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,147 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            if (Char.IsLetter(c))
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            for(i = 0; i < vals.Length; i++) 
+            {
+                continue;
+            }
+            if(i % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if(number % 2 == 0) 
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if(num % 2 != 0) 
+            {
+                return true;
+            }
+            return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if( numbers == null)
+            {
+                return 0;
+            }
+            if (numbers.Count() ==0)
+            {
+                return 0;
+            }
+
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if(str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+              return str1.Length;
+            
+            /*int i = str1.Length;
+            int j = str2.Length;
+            if(i > j)
+            {
+                return j;
+            }
+            return i;*/
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null)
+            {
+                return 0;
+            }
+            int a = 0;
+            for(int i = 0; i < numbers.Length; i++) 
+            {
+                a += numbers[i];
+            }
+            return a;
+            /*int i = numbers.Length;
+            int j = 0;
+            int a = 0;
+            while(j< i) 
+            {
+                a += numbers[j];
+                j++;
+            }
+            return a;*/
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int total = 0;
+            for(int i = 0; i < numbers.Length;i++)
+            {
+                if (numbers[i] % 2 == 0) 
+                {
+                    total+= numbers[i];
+                }
+            }
+            return total;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) 
+            {
+                return false;
+            }
+            int total = 0;
+            for(int i = numbers.Count -1; i >= 0; i--) 
+            {
+                if (numbers[i] % 2 != 0) 
+                {
+                    total+= numbers[i];
+                }
+            }
+             if(total % 2 != 0)
+            {
+                return true;
+            }
+             return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+            }
         }
     }
 }
