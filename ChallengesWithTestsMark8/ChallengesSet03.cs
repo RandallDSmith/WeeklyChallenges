3d09e519dcab29db2fd673054d950fd0e8ffd15e
Randall Smith <rdsmith1761@gmail.com>
2023-03-11 08:16:43 -0500

    Challenge set 4

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -9,112 +9,113 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            for(int i =0; i < vals.Length; i++)
+          for(int i = 0; i < vals.Length; i++)
             {
                 if (vals[i] == false)
                 {
-                    return true;
+                    return false;
                 }
                 
             }
-            return false;
+            return true;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            if(numbers == null || numbers.Count() == 0)
+            if (numbers.Count() == 0 || numbers == null)
             {
                 return false;
             }
             var sum = numbers.Sum();
 
-           if(sum % 2 != 0)
-            {
-                return true;
-            }    
-           return false;
-        }
-
-        public bool PasswordContainsUpperLowerAndNumber(string password)
-        {
-            var isLower = false;
-            var isUpper = false;
-            var isNumber = false;
-
-            for(int i = 0; i < password.Length; i++)
-            {
-                if (char.IsLower(password[i]))
-                {
-                    isLower = true;
-                }
-                if (char.IsUpper(password[i]))
-                {
-                    isUpper = true;
-
-                }
-                if (char.IsNumber(password[i]))
-                {
-                    isNumber = true;
-                }
-            }
-            if(isLower == true && isUpper == true && isNumber == true)
+            if (sum % 2 != 0)
             {
                 return true;
             }
             return false;
         }
 
-        public char GetFirstLetterOfString(string val)
-        {
-             
-            return val.First();
-        }
+            public bool PasswordContainsUpperLowerAndNumber(string password)
+             {
 
-        public char GetLastLetterOfString(string val)
-        {
-            return val[val.Length - 1];
-        }
+                     var cUpper = false;
+                     var cLower = false;
+                     var cNumber = false;
 
-        public decimal Divide(decimal dividend, decimal divisor)
-        {
-            if(divisor== 0)
-            {
-                return 0;
+                     for(int i = 0; i < password.Count();i++)
+                     {
+                         if (char.IsLower(password[i]))
+                         {
+                             cUpper = true;
+                         }
+                         if (char.IsUpper(password[i]))
+                         {
+                             cLower = true;
+                         }
+                         if (char.IsNumber(password[1]))
+                         {
+                             cNumber = true;
+                         }
+
+                         if(cUpper == true && cLower == true && cNumber == true)
+                         {
+                             return true;
+                         }
+                             return false;
+                     }
+            return false;
             }
-            return dividend / divisor;
-        }
 
-        public int LastMinusFirst(int[] nums)
-        {
-            if(nums.Length == 0)
-            {
-                return 0;
-            }
+             public char GetFirstLetterOfString(string val)
+             {
+                  return val.First();
 
-            return nums[nums.Length - 1] - nums[0];
-        }
+             }
 
-        public int[] GetOddsBelow100()
-        {
-            var list = new List<int>();
+             public char GetLastLetterOfString(string val)
+             {
+                return val[val.Length - 1];
+             }
 
-            for(int i = 0; i < 100; i++)
-            {
-                if(i % 2 != 0) 
+             public decimal Divide(decimal dividend, decimal divisor)
+             {
+                if(divisor == 0)
+                     {
+                         return 0;
+                     }
+
+                return dividend / divisor;
+             }
+
+             public int LastMinusFirst(int[] nums)
+             {
+                     return nums[nums.Length - 1] - nums[0];
+             }
+
+             public int[] GetOddsBelow100()
+             {
+                     int[] i = new int[49];
+                     int a = 0;
+                     for(int x = 0; x <100; x++)
+                     {
+                         if(x % 2 != 0)
+                         {
+                             i[a] = x;
+                             a++;
+                         }
+                     }
+                     return i;
+             }
+
+             public void ChangeAllElementsToUppercase(string[] words)
                 {
-                   list.Add(i);
-                }
-            }
-            return list.ToArray();
-        }
-
-        public void ChangeAllElementsToUppercase(string[] words)
-        {
-            for(int i = 0; i < words.Length;i++)
-            {
-                words[i] = words[i].ToUpper();
-            }
+                    for(int i = 0; i < words.Length; i++)
+                    {
+                            words[i] = words[i].ToUpper();
+                    }
+           
             
-        }
+                }
+        
     }
 }
